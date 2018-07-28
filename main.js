3a68231889150ca981872e0c97ff5a8e1ef6c9bf
Arek Ouzounian <20ouzouniana@prepstudents.org>
2018-07-28 13:21:05 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,19 @@ function initError() {
 function capture(payload) {
     score.textContent = payload.score;
 }
+//code for text-to-speech
+/*
+ex:
+var tts = new SpeechSynthesisUtterance('Example');
+window.speechSynthesis.speak(tts);
+tts.volume = 1;
+tts.rate = 1;  //can be between 0.1 - 10
+tts.text = 'Example';
+
+tts.onend = function(e) {
+    console.log('Finished in ' + event.elapsedTime + 'seconds.');
+}
+*/
 
 DiffCamEngine.init({
     video: video,
