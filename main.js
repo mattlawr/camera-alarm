e0644353ad12c8ef6f591c4dad33d23ff1035e22
Matthew Lawrence <mattblawr@gmail.com>
2018-07-28 10:20:20 -0700

    Add diff-cam support

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-var a = 1000;
+var video = document.getElementById('video');
+var canvas = document.getElementById('motion');
+var score = document.getElementById('score');
+
+function initSuccess() {
+    DiffCamEngine.start();
+}
+
+function initError() {
+    alert('Something went wrong.');
+}
+
+function capture(payload) {
+    score.textContent = payload.score;
+}
+
+DiffCamEngine.init({
+    video: video,
+    motionCanvas: canvas,
+    initSuccessCallback: initSuccess,
+    initErrorCallback: initError,
+    captureCallback: capture
+});
